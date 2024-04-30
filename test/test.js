f15d10e4504829ef9653aaaa1aaa80c331fdece3
Tamim Shahriar <tash3080@gmail.com>
2024-05-01 01:55:32 +1000

    Commit for Prac 6

--- test/test.js
+++ test/test.js
@@ -45,36 +45,8 @@ describe('Flower Unit Test', function () {
 
                 expect(responseData.message).to.equal("Successful")
 
-                //expect(responseData.data[0]["id"]).to.equal("1");
-                //expect(responseData.data[0]["name"]).to.equal("My Cat");
                 done();
             });
         });
     });
-
-    // describe('Delete Flower', () => {
-    //     it('should delete a flower', (done) => {
-    //         const deleteData = {
-    //             id: '1',
-    //             name: 'My Flower'
-    //         };
-
-    //         request.delete({
-    //             url: url,
-    //             json: deleteData
-    //         }, (error, response, body) => {
-    //             if (error) {
-    //                 done(error);
-    //                 return;
-    //             }
-
-    //             const responseData = body;
-    //             expect(response.statusCode).to.equal(200);
-
-    //             expect(responseData.message).to.equal("delete flower success")
-
-    //             done();
-    //         });
-    //     });
-    // });
 });
